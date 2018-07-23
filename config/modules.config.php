45d1858bed58453c94ba0b769de9ccec200edc98
Tom H Anderson <tom.h.anderson@gmail.com>
2018-07-23 17:24:38 -0600

    composer

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,9 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\ServiceManager\Di',
+    'Zend\Mvc\I18n',
+    'Zend\Mvc\Console',
     'Zend\Router',
     'Zend\Validator',
     'Application',
